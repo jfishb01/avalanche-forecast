dd4efba8a94602efbedbff205651b496ebdba462
Josh Fishbein <joshua.p.fishbein@gmail.com>
2024-11-04 20:24:03 -0600

    Fix broken unit tests

--- src/ml/training/evaluation/metrics.py
+++ src/ml/training/evaluation/metrics.py
@@ -24,15 +24,17 @@ def _get_evaluation_classifier_metrics(
             (evaluation_set["distributor"] == area.distributor)
             & (evaluation_set["area_id"] == area.area_id)
         ]
-        eval_metrics_by_area[f"accuracy - {area.distributor}.{area.area_name}"] = (
+        eval_metrics_by_area[f"accuracy - {area.distributor}.{area.area_name}"] = round(
             metrics.accuracy_score(
                 evaluation_set_by_area["observed"], evaluation_set_by_area["predicted"]
-            ).round(3)
+            ),
+            3,
         )
 
-    eval_metrics_aggregate["accuracy"] = metrics.accuracy_score(
-        evaluation_set["observed"], evaluation_set["predicted"]
-    ).round(3)
+    eval_metrics_aggregate["accuracy"] = round(
+        metrics.accuracy_score(evaluation_set["observed"], evaluation_set["predicted"]),
+        3,
+    )
     eval_metrics_aggregate["min_accuracy"] = min(eval_metrics_by_area.values())
     eval_metrics_aggregate["max_accuracy"] = max(eval_metrics_by_area.values())
     return dict(**eval_metrics_aggregate, **eval_metrics_by_area)
